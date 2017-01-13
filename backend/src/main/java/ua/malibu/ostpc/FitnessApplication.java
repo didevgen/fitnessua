9dee4d8dba21b15bda4f3a0fad7380fc408399f3
Eugene Kovaljov <e.kovaljov@infostroy.com.ua>
2017-01-13 15:18:21 +0200

    permanent commit

--- backend/src/main/java/ua/malibu/ostpc/FitnessApplication.java
+++ backend/src/main/java/ua/malibu/ostpc/FitnessApplication.java
@@ -3,8 +3,11 @@ package ua.malibu.ostpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import ua.malibu.ostpc.config.DatabaseConfig;
 
 @SpringBootApplication
+@Import({DatabaseConfig.class})
 @ComponentScan("ua.malibu.ostpc.controllers")
 public class FitnessApplication {
 
