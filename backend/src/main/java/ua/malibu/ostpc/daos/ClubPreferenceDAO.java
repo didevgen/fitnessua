a46fb1538cb1d3db1c06b51555b7d2cf5a44e1cf
IgorKulik <ihor.kulik@nure.ua>
2017-02-09 20:31:51 +0200

    Fixed DAOs

--- backend/src/main/java/ua/malibu/ostpc/daos/ClubPreferenceDAO.java
+++ backend/src/main/java/ua/malibu/ostpc/daos/ClubPreferenceDAO.java
@@ -1,12 +1,16 @@
 package ua.malibu.ostpc.daos;
 
 import com.querydsl.jpa.impl.JPAQuery;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ua.malibu.ostpc.models.ClubPreference;
 import ua.malibu.ostpc.models.QClubPreference;
 
 /**
  * Created by Игорь on 09.02.2017.
  */
+@Repository("clubPreferenceDao")
+@Transactional
 public class ClubPreferenceDAO extends BaseDAO<ClubPreference> {
     @Override
     public ClubPreference get(String uuid) {
